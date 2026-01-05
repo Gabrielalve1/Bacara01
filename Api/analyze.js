79cc9618b6f12b171c21d06d3bd65bc876f2a796
Gabrielalve1 <159019094+Gabrielalve1@users.noreply.github.com>
2026-01-05 01:31:13 -0300

    Update analyze.js

--- Api/analyze.js
+++ Api/analyze.js
@@ -1,1 +1,8 @@
-
+export default function handler(req, res) {
+  res.status(200).json({
+    status: "ok",
+    mensagem: "API funcionando 🚀",
+    recomendacao: "AZUL",
+    confianca: "70%"
+  });
+}
