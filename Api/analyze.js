168faffb5d752ca542c5d08d2f13a16a80a3ae63
Gabrielalve1 <159019094+Gabrielalve1@users.noreply.github.com>
2026-01-05 01:41:48 -0300

    Delete Api directory

--- Api/analyze.js
+++
@@ -1,8 +0,0 @@
-export default function handler(req, res) {
-  res.status(200).json({
-    status: "ok",
-    mensagem: "API funcionando 🚀",
-    recomendacao: "AZUL",
-    confianca: "70%"
-  });
-}
